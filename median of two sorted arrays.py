d569ee0f3ecaa466bbabce66123d5bd7d04a5e9b
Srikanishk <108481316+Srikanishk2003@users.noreply.github.com>
2023-03-14 16:16:40 +0530

    Add files via upload

---
+++ median of two sorted arrays.py
@@ -0,0 +1,20 @@
+from math import ceil
+def median_of_two_arrays(l, array):
+    if (l % 2) == 0:
+        medium = l // 2
+        median = (array[medium] + array[medium - 1]) / 2
+        return median
+    else:
+        medium = ceil(l / 2)
+        return array[medium - 1]
+
+
+array1 = [1, 10, 15, 26, 38]
+array2 = [5, 11, 17, 35, 45]
+
+print("Array after merging and sorting :", end="")
+array1 += array2
+array1.sort()
+print(array1)
+ans = median_of_two_arrays(len(array1), array1)
+print("Median of the arrays is  :", int(ans))
